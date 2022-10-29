c53d82c576df32f170cc6c7500068f834051900e
frankiehuangg <frankie.huangg@gmail.com>
2022-10-29 12:25:01 +0700

    add: ConnectParent test case

--- lib/tree/driver.c
+++ lib/tree/driver.c
@@ -8,31 +8,35 @@ int main()
 	printf("Is empty: %d\n", HasNoChild(t));
 
 	Tree c1 = NewTree(2, Nil);
-	ConnectTree(c1, &t);
+	ConnectChild(c1, &t);
 	PrintTree(t); printf("\n");
 	printf("Is one element: %d\n", HasOneChild(t));
 
 	Tree c2 = NewTree(3, Nil);
-	ConnectTree(c2, &t);
-	PrintTree(t); printf("\n");
-	printf("Is empty: %d\n", HasNoChild(t));
-
-	Tree c3 = NewTree(4, Nil);
-	ConnectTree(c3, &t);
-	PrintTree(t); printf("\n");
-	printf("Is one element: %d\n", HasOneChild(t));
-
-	Tree c4 = NewTree(5, Nil);
-	ConnectTree(c4, &t);
+	ConnectParent(c2, &t);
 	PrintTree(t); printf("\n");
 
-	Tree c5 = NewTree(6, Nil);
-	ConnectTree(c5, &t);
-	PrintTree(t); printf("\n");
+	// Tree c2 = NewTree(3, Nil);
+	// ConnectChild(c2, &t);
+	// PrintTree(t); printf("\n");
+	// printf("Is empty: %d\n", HasNoChild(t));
 
-	Tree c6 = NewTree(7, Nil);
-	ConnectTree(c6, &t);
-	PrintTree(t); printf("\n");
+	// Tree c3 = NewTree(4, Nil);
+	// ConnectChild(c3, &t);
+	// PrintTree(t); printf("\n");
+	// printf("Is one element: %d\n", HasOneChild(t));
+
+	// Tree c4 = NewTree(5, Nil);
+	// ConnectChild(c4, &t);
+	// PrintTree(t); printf("\n");
+
+	// Tree c5 = NewTree(6, Nil);
+	// ConnectChild(c5, &t);
+	// PrintTree(t); printf("\n");
+
+	// Tree c6 = NewTree(7, Nil);
+	// ConnectChild(c6, &t);
+	// PrintTree(t); printf("\n");
 
 	DeallocateTree(t);
 }
