33f9226727c5be7911a23917b2e5ddbdaefa33f2
frankiehuangg <frankie.huangg@gmail.com>
2022-10-31 20:29:42 +0700

    new: catalog

---
+++ program/lib/catalog/catalog.h
@@ -0,0 +1,10 @@
+#ifndef CATALOG
+#define CATALOG
+
+void DisplayCatalog(ListMakanan* Makanan);
+/*  Menampilkan catalog pada layar (berisi nama, waktu kadaluwarsa
+	aksi, dan waktu pengiriman
+	I.S. : Makanan terdefinisi
+	F.S. : Catalog tercetak pada layar */
+
+#endif CATALOG
