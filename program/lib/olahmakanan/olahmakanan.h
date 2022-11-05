7951115db1b5b427d98e99e5d54744edce8f39c5
Alex Sander <maximun154@gmail.com>
2022-11-05 11:22:31 +0700

    update processlist

--- program/lib/olahmakanan/olahmakanan.h
+++ program/lib/olahmakanan/olahmakanan.h
@@ -2,6 +2,8 @@
 #define OLAHMAKANAN_H
 
 #include "../../lib/queue/queue.h"
+#include "../../../lib/queue/prioqueuetime.h"
+#include "../../lib/deliverylist/deliverylist.h"
 #include "../../lib/string/string.h"
 #include "../../lib/matrix/matrix.h"
 #include "../../lib/liststatik/liststatik.h"
@@ -11,16 +13,32 @@
 #include "../resep/resep.h"
 #include <stdio.h>
 
-void displayMakananFiltered(String aksi, ListMakanan *lfood, ListMakanan *lfiltered);
+typedef PrioQueueTime ProcessList;
+
+void process(String aksi,int i, ListMakanan *lfood, Inventory I, ListMakanan *lfiltered, ProcessList *P, ResepTree Resep);
 /*I.S. lfiltered sembarang*/
 /*F.S. lfiltered yang berisi elemen-elemen dari lfood yang dapat dilakukan aksi*/
 
 boolean isBahanAvailable(ListMakanan lfood, ListMakanan lneed);
 /*mengembalikan nilai true apabila semua bahan yang diperlukan untuk memproses makanan tersedia*/
 
-void process(String aksi,int i, ListMakanan *lfood, ListMakanan *lfiltered, Inventory *I, ResepTree Resep);
+void process(String aksi,int i, ListMakanan *lfood, Inventory I, ListMakanan *lfiltered, ProcessList *P, ResepTree Resep);
 /*I.S. lfiltered sembarang, lfood*/
 /*F.S. jika semua bahan tersedia, bahan makanan yang dibuat bertambah dan bahan makanan yang diperlukan berkurang di inventory,
        jika ada bahan yang tidak tersedia, menampilkan bahan-bahan yang tidak tersedia tersebut*/
+void CreateProcessMakanan(ProcessList *P,int max);
 
+void updateProcessList(ProcessList *P, Inventory *I, TIME t, ListMakanan *delivered);
+
+void minusProcessTime(ProcessList *P, TIME t);
+
+void plusProcessTime(ProcessList *P, TIME t);
+
+int lengthProcessList(ProcessList P);
+
+boolean checkUpgrade(ProcessList P);
+
+void upgradeProcessList(ProcessList *P);
+
+void displayProcessList(ProcessList P);
 #endif
