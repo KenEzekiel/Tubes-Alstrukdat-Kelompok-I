5c39814e676d863d35a322af8e2268364404b1ec
frankiehuangg <frankie.huangg@gmail.com>
2022-11-07 00:23:43 +0700

    fix: file path

--- program/main.c
+++ program/main.c
@@ -32,17 +32,17 @@ void InitializeVariables()
 	CreateKulkas(&K, 20, 30);
 
 	// // Initialize constant variables (read from file)
-	ReadFromFile(&Resep, 10, "../test/resep_2.txt");
-	StartPeta(&Map, "../test/peta.txt");
+	ReadFromFile(&Resep, 10, "test/resep_2.txt");
+	StartPeta(&Map, "test/peta.txt");
 	CreateListMakanan(&DaftarMakanan);
-	DaftarMakanan = readListMakanan("../test/makanan.txt");
+	DaftarMakanan = readListMakanan("test/makanan.txt");
 
 	PushUndoStack(&US, State(BNMO));
 }
 
 int main()
 {
-	displaySplashScreen("lib/utility/SplashScreen.txt");
+	displaySplashScreen("program/lib/utility/SplashScreen.txt");
 	printf("User input: ");
 	STARTWORD();
 
