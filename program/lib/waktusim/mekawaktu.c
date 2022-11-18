ca8b8ab69ee91cd853a28435c0e98dd73036b1c2
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2022-11-19 01:25:24 +0700

    change urutan update

--- program/lib/waktusim/mekawaktu.c
+++ program/lib/waktusim/mekawaktu.c
@@ -5,15 +5,15 @@ void UpdateActionTime(State *S){
     TIME t;
     CreateTime(&t,0,0,1);
     updateProcessList(&ProcessedList(*S),&InventoryState(*S),t, &DeliveredListState(*S));
-    updateInventory(&InventoryState(*S),t, &ExpListState(*S));
     updateDeliveryList(&DeliveryListState(*S),&InventoryState(*S),t, &DeliveredListState(*S));
+    updateInventory(&InventoryState(*S),t, &ExpListState(*S));
     UpdateGlobalTime(&TimeState(*S),t);
 }
 
 void UpdateWaitTime(State *S, TIME t){
     updateProcessList(&ProcessedList(*S),&InventoryState(*S),t, &DeliveredListState(*S));
-    updateInventory(&InventoryState(*S),t, &ExpListState(*S));
     updateDeliveryList(&DeliveryListState(*S),&InventoryState(*S),t, &DeliveredListState(*S));
+    updateInventory(&InventoryState(*S),t, &ExpListState(*S));
     UpdateGlobalTime(&TimeState(*S),t);
 }
 
